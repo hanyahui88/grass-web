721bd2b073ffbfa468ccc4ff975e464385a83ede
alvin <hanyahui88@163.com>
2016-05-20 17:26:45 +0800

    修改跨域头

--- src/main/java/com/grass/common/filter/CommonFilter.java
+++ src/main/java/com/grass/common/filter/CommonFilter.java
@@ -16,14 +16,10 @@ public class CommonFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        //  HttpServletRequest request = (HttpServletRequest) servletRequest;
-        //  HttpSession session = request.getSession();
-        // 指定允许其他域名访问
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-        // 响应头设置
-        response.addHeader("Access-Control-Allow-Headers", "POWERED-BY-FANTONG");
-        response.addHeader("Access-Control-Max-Age", "30");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         // 需要过滤的代码
         filterChain.doFilter(servletRequest, servletResponse);
     }
